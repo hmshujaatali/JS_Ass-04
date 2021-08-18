a8662887d01bc1f93a699a11a848e1ea8805f177
hmshujaatali <81732578+hmshujaatali@users.noreply.github.com>
2021-08-18 15:11:36 +0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,18 @@
+// // var a1=["b","b","d"]
+// // document.write(a1,"<br/>")
+// // a1.pop()
+// // document.write(a1,"<br/>")
+// // a1.push("c")
+// // document.write(a1,"<br/>")
+// // a1.shift()
+// // document.write(a1,"<br/>")
+// // a1.unshift("a")
+// // document.write(a1,"<br/>" )
+// // var a=["0","1","2","3","4","5","6"]
+// // a.splice(2,2,"000","222")
+// // alert(a)
+// var q=["1","3","5","7","9"]
+// document.write(q,"<br/>")
+// var q1=q.splice(1,0,"2")
+// document.write(q,"<br/>")
+// document.write(q1,"<br/>")
---
+++ app.js
@@ -0,0 +1,112 @@
+//                 QUESTION#01
+// var studentNames=["","","","","","","","","","","","","","","","",]
+//                  QUESTION#02
+// var studentNames=["","","","","","","","","","","","","","","","",]
+                    // QUESTION#03
+// var Cities=["karachi","hyderabad","noriabad","larkana","khatmandu","tandoaalayar","sukkur","khairpur",]
+                    // QUESTION#04
+// var numbers=[1,2,3,4,5,6,7,8]
+                    // QUESTION#05
+// var bolean=["+","-","*","/","%"]
+                    // QUESTION#06
+// var array=["a","b",1,3,5,"edd"]
+                    // QUESTION#07
+// var degrees=["SSC","HSC","BSC","BS","BCOM","BE","MS","M.PHIL","PhD"]
+// document.write("<h1>Qualifications:</h1>","<br/>")
+// document.write("1) ",degrees[0],"<br/>")
+// document.write("2) ",degrees[1],"<br/>")
+// document.write("3) ",degrees[2],"<br/>")
+// document.write("4) ",degrees[3],"<br/>")
+// document.write("5) ",degrees[4],"<br/>")
+// document.write("6) ",degrees[5],"<br/>")
+// document.write("7) ",degrees[6],"<br/>")
+// document.write("8) ",degrees[7],"<br/>")
+// document.write("9) ",degrees[8],"<br/>")
+                    // QUESTION#08
+// var studentNames=["ali","asim","ahmed"]
+// var studentScore=[320,230,480]
+// var studentTotalMarks=[500]
+// var st1=(studentScore[0]/studentTotalMarks[0])*100
+// var st2=(studentScore[1]/studentTotalMarks[0])*100
+// var st3=(studentScore[2]/studentTotalMarks[0])*100
+// document.write("Score of Ali is ",studentScore[0],". Percentage: ",st1,"%","<br/>",
+// "Score of Asim is ",studentScore[1],". Percentage: ",st2,"%","<br/>",
+// "Score of Ahmed is ",studentScore[2],". Percentage: ",st3,"%","<br/>")
+                    // QUESTION#09
+// var colorName=["red","green","blue","yellow"]
+// document.write(colorName)
+// var a1=prompt("Which color You want to add at the beginning")
+// colorName.unshift(a1)
+// document.write("<br/>",colorName)
+// var a2=prompt("Which color You want to add at the end")
+// colorName.push(a2)
+// document.write("<br/>",colorName)
+// colorName.unshift("Pruple","Blue")
+// document.write("<br/>",colorName)
+// colorName.shift()
+// document.write("<br/>",colorName)
+// colorName.pop()
+// document.write("<br/>",colorName)
+// var a3=prompt("Which color You want to add in the array","<br/>")
+// var a4=+prompt("At Which position You want to add",a3,"color","<br/>")
+// colorName.splice(a4,"0",a3)
+// document.write("<br/>",colorName,"<br/>",colorName.length)
+// var a5=+prompt("At Which position You want to delete the color")
+// var a6=+prompt("How many colors you want to delete")
+// colorName.splice(a5,a6)
+// document.write("<br/>",colorName,"<br/>",colorName.length)
+                    // QUESTION#10
+// var studentsScore=['320',"230","480","120"]
+// studentsScore.sort()
+// document.write(studentsScore)
+                    // QUESTION#11
+// var cityNames=["karachi","hyderabad","noriabad","kasoor","multan","peshawar","tandoadam"]
+// document.write("Cities list: ",cityNames,"<br/>")
+// var selectedCities=cityNames.slice(0,3)
+// document.write("Selected Cities: ",selectedCities)
+                    // QUESTION#12
+// var array = ["This ","is ","my ","cat"];
+// var ar=array.join("")
+// document.write(array,"<br/>")
+// document.write(ar)
+                    // QUESTION#13
+// var devices=["First","In","First","Out"]
+// var a=devices.join("")
+// document.write(a)
+
+                    // QUESTION#14
+// var devices=["First","In","LasT","Out"]
+// var a=devices.reverse()
+// document.write(a)
+                    // QUESTION#15
+// var phone=["Apple", "Samsung", "Motorola", "Nokia", "Sony" , "Haier"]
+// document.write("<select>",
+// "<option>",phone[0],"</option>","<br/>",
+// "<option>",phone[1],"</option>","<br/>",
+// "<option>",phone[2],"</option>","<br/>",
+// "<option>",phone[3],"</option>","<br/>",
+// "<option>",phone[4],"</option>","<br/>",
+// "</select>")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
